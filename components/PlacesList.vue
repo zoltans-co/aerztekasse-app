06338601f80a05b91130fce1d82991cd89480119
zoltan <mail@zoltans.co>
2025-04-02 14:44:31 +0200

    Updating style

--- components/PlacesList.vue
+++ components/PlacesList.vue
@@ -20,6 +20,7 @@
     fetchPlaces();
   });
 
+
 </script>
 
 <template>
@@ -29,16 +30,18 @@
   </header>
 
   <!-- Verify if there is any data -->
-  <div v-if="placesData">
+  <div class="places-list" v-if="placesData">
     <!-- Iterate over the places -->
-    <div v-for="(place, index) in placesData" :key="index" class="place">
-      <h2>{{ place.label }}</h2>
-      <p><strong>Address:</strong> {{ place.location }}</p>
+    <div v-for="(place, index) in placesData" :key="index" class="place places-item">
+      <div class="place-header">
+        <h2>{{ place.label }}</h2>
+        <p><strong>Address:</strong> {{ place.location }}</p>
+      </div>
 
       <div class="opening-hours">
         <h3>Opening hours:</h3>
         <div v-for="(hours, day) in place.opening_hours.days" :key="day">
-          <p><strong>{{ day }}</strong>:</p>
+          <p class="day-header"><strong>{{ day }}</strong>:</p>
           <ul>
             <!-- If there is no opening hours -->
             <li v-if="hours.length === 0">Closed</li>
@@ -54,9 +57,9 @@
   </div>
 
 
-  <!-- Üres állapot kezelése -->
+  <!-- Loading state -->
   <div v-else>
-    <p>Adatok betöltése folyamatban...</p>
+    <p>Loading data in progress...</p>
   </div>
 
 </template>
@@ -160,10 +163,8 @@
     scale: 1.1;
   }
 
-  .content {
+  .places-list {
     margin-top: 10em;
-    /*background-color: #f4f4f5;*/
-    /*border: 4px solid black;*/
     min-height: 800px;
 
     display: flex;
@@ -172,4 +173,56 @@
     justify-content: space-evenly;
     /*gap: 1em;*/
   }
+
+  .places-item {
+    display: flex;
+    align-items: center; /* Igazítás függőlegesen középre */
+    gap: 10px; /* Távolság az elemek között */
+
+    background-color: #EEEEEE;
+    border-radius: 10px;
+
+    margin: 1rem;
+
+    width: 70%;
+
+    display: flex;
+    align-items: flex-start;
+
+  }
+
+  .place-header {
+    font-size: 1.2rem;
+    font-weight: bold;
+    text-align: left;
+
+    vertical-align: top;
+
+    width: 30%;
+    padding: 3rem 3rem 3rem 5rem;
+  }
+
+  .opening-hours {
+    font-size: 1.2rem;
+    color: #666;
+    text-align: left;
+
+    background-color: #DDDDDD;
+
+    width: 70%;
+    padding: 3rem 3rem 3rem 5rem;
+
+  }
+
+  ul {
+    list-style-type: none; /* Eltávolítja a pöttyöket */
+    margin: 0; /* Opcionális: alapértelmezett margó eltávolítása */
+    padding: 0; /* Opcionális: alapértelmezett behúzás eltávolítása */
+  }
+
+  .day-header {
+    padding-top: 0.7rem;
+    padding-bottom: 0.2rem;
+  }
+
 </style>
